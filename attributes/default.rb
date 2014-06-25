d77f08fadc13f3554c8e5a52143a2d9fb706a182
Adam Krone <krone.adam@gmail.com>
2014-06-25 09:45:34 -0500

    Moves ruby version into attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 default['capistrano_ruby']['app_name'] = 'app'
 default['capistrano_ruby']['environment'] = 'production'
+default['capistrano_ruby']['ruby_version'] = 'ruby-2.1.2'
 default['capistrano_ruby']['deployment_user'] = 'deploy'
 default['capistrano_ruby']['deployment_group'] = 'www-data'
 
--- recipes/rvm.rb
+++ recipes/rvm.rb
@@ -1,6 +1,6 @@
 node.normal['rvm']['user_installs'] = [
   { 'user' => node['capistrano_ruby']['deployment_user'],
-    'default_ruby' => 'ruby-2.1.2'}
+    'default_ruby' => node['capistrano_ruby']['ruby_version']}
 ]
 
 include_recipe 'rvm::user'
